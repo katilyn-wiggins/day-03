3ca0d3633c3c08ebb59ee4edde106f0baec7afee
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-03-25 10:39:48 -0300

    wrote get tests

--- order-delivery/__tests__/app.test.js
+++ order-delivery/__tests__/app.test.js
@@ -42,33 +42,40 @@ describe('03_separation-of-concerns-demo routes', () => {
     });
   });
 
-  describe('put function w/ .then', () => {
-    it('updates an order in our database and sends a text message', () => {
-      return request(app)
-        .put('/api/v1/orders')
-        .send({ quantity: 8 })
-        .then((res) => {
-          expect(createMessage).toHaveBeenCalledTimes(1);
-          expect(res.body).toEqual({
-            id: '1',
-            quantity: 8,
-          });
-        });
+  describe('get function w/ async await', async () => {
+    it('ASYNC/AWAIT: gets all orders in our database', async () => {
+      const res = await request(app).get('/api/v1/orders');
+
+      expect(res.body).toEqual({
+        id: '1',
+        quantity: 10,
+      });
     });
   });
 
-  describe('put function w/ .then', () => {
-    it('updates an order in our database and sends a text message', () => {
-      return request(app)
-        .put('/api/v1/orders/1')
-        .send({ quantity: 8 })
-        .then((res) => {
-          expect(createMessage).toHaveBeenCalledTimes(1);
-          expect(res.body).toEqual({
-            id: '1',
-            quantity: 8,
-          });
-        });
+  describe('get by id function w/ async await', async () => {
+    it('ASYNC/AWAIT: gets all orders in our database with id 1', async () => {
+      const res = await request(app).get('/api/v1/orders/1');
+
+      expect(res.body).toEqual({
+        id: '1',
+        quantity: 10,
+      });
     });
   });
+
+  // describe('put function w/ .then', () => {
+  //   it('updates an order in our database and sends a text message', () => {
+  //     return request(app)
+  //       .put('/api/v1/orders')
+  //       .send({ quantity: 8 })
+  //       .then((res) => {
+  //         expect(createMessage).toHaveBeenCalledTimes(1);
+  //         expect(res.body).toEqual({
+  //           id: '1',
+  //           quantity: 8,
+  //         });
+  //       });
+  //   });
+  // });
 });
